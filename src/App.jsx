d3680bf99233e9a9f98648a23b25e40a3e56f38e
md nasique <nasiquejahangir000@gmail.com>
2025-10-02 15:25:12 +0530

    Update project code

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from './component/Navbar';
 import TextForm from './component/TextForm';
-// import About from './component/About';
+import About from './component/About';
 import Alert from './component/Alert';
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes
+} from "react-router-dom";
+
+
 
 function App() {
     const [mode, setMode] = useState('light');
@@ -11,41 +19,52 @@ function App() {
 
 
     const showAlert = (message, type) => {
-        setAlert({msg: message, type: type})
+        setAlert({ msg: message, type: type })
         setTimeout(() => {
             setAlert(null)
         }, 1500)
     }
 
-    
-
-
     // whether dark mode is enabled or not
     const togglemode = () => {
         if (mode === 'dark') {
-            setMode('light')
+            setMode('light');
             document.body.style.backgroundColor = 'white';
             showAlert("Light mode has been enabled", "success")
 
         } else {
-            setMode('dark')
+            setMode('dark');
             document.body.style.backgroundColor = '#042743';
             showAlert("Dark mode has been enabled", "success")
         }
     }
+
+
+
+
+
+
     return (
-        <>
-            <Navbar name="worknape"
-                mode={mode}
-                togglemode={togglemode}/>
 
-            <Alert alert={alert}
-                />
-
+        <Router>
+            <Navbar name="Textutils" mode={mode} togglemode={togglemode} />
+            <Alert alert={alert} />
             <div className="container my-3">
-                <TextForm heading="This is props heading"
-                    mode={mode}  alert={alert} showAlert={showAlert}/> {/* <About mode={mode} /> */} </div>
-        </>
+        <Routes>
+          {/* <Route path="/about" exact element={<About />}>
+            
+          </Route> */}
+
+          <Route path="/" exact element={<TextForm heading="This is props heading" mode={mode} alert={alert} showAlert={showAlert} />}>
+            
+          </Route>
+        </Routes>
+                {/* <Routes>
+                    <Route path="/about" element={<About />} />
+                    <Route path="/" element={<TextForm heading="This is props heading" mode={mode} showAlert={showAlert} />} />
+                </Routes> */}
+            </div>
+        </Router>
     );
 }
 
